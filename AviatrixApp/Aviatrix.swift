18e29377e4853b067ab06a3e1dc31ddb75e1cefd
Marnie Reid <marniejeanreid@gmail.com>
2019-05-10 21:49:16 -0700

    Initial commit

---
+++ AviatrixApp/Aviatrix.swift
@@ -0,0 +1,41 @@
+//
+//  Aviatrix.swift
+//  AviatrixApp
+//
+//  Created by Amy Holt on 6/10/18.
+//  Copyright © 2018 Amy Holt. All rights reserved.
+//
+
+import Foundation
+
+class Aviatrix {
+    
+    var author = " "
+    
+    init (authorName : String){
+        author = authorName
+    }
+    
+    var running = false
+    
+    func start() -> Bool {
+        running = true
+        return running
+    }
+    
+    func refuel() {
+        
+    }
+    
+    func flyTo(destination : String) {
+        
+    }
+    
+    func distanceTo(target : String) {
+    
+    }
+    
+    func knownDestinations() -> [String] {
+       return ["St. Louis"]
+    }
+}
